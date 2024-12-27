fdf6ced959e8a6c3704effac7e63db4c551c73f1
Zahra Asgari <152491883+ZahraAsgari379@users.noreply.github.com>
2024-12-28 01:01:52 +0330

    Update inbox.js

--- Inbox/inbox.js
+++ Inbox/inbox.js
@@ -44,87 +44,119 @@ $(document).ready(function () {
       console.error("اطلاعات یافت نشد");
     },
   });
-});
 
-// menu
-
-$(document).on("click", ".menu-btn", function () {
-  var $row = $(this).closest("tr");
-  var menu = $('<div class="context-menu">')
-    .append(
-      '<button class="cancel-btn"><i class="fa fa-close" style="font-size:18px"></i></button>'
-    )
-    .append('<div class="menu-item show-row">مشاهده</div>')
-    .append('<div class="menu-item delete-row">حذف</div>');
-
-  $("body").append(menu);
-  menu.css({
-    top: $(this).offset().top,
-    left: $(this).offset().left,
-  });
-
-  $(".fa-close").click(function () {
-    menu.remove();
-  });
-
-  // card
-
-  $(".show-row").on("click", function () {
-    var selectedData =
-      "فرستنده: " + $row.find("td:nth-child(3)").text() + "<br>";
-    selectedData += "سمت: " + $row.find("td:nth-child(4)").text() + "<br>";
-    selectedData += "نوع مدرک: " + $row.find("td:nth-child(5)").text() + "<br>";
-    var card = $('<div id="inbox--details"></div>')
+  $(document).on("click", ".menu-btn", function () {
+    var $row = $(this).closest("tr");
+    var menu = $('<div class="context-menu">')
       .append(
-        '<button class="cancel-btn"><i class="fa fa-close" style="font-size:24px"></i></button>'
+        '<button class="cancel-btn"><i class="fa fa-close" style="font-size:18px"></i></button>'
       )
-      .append(selectedData);
-    menu.remove();
+      .append('<div class="menu-item show-row">مشاهده</div>')
+      .append('<div class="menu-item delete-row">حذف</div>');
 
-    $("body").append(card);
+    $("body").append(menu);
+    menu.css({
+      top: $(this).offset().top + $(this).height(),
+      left: $(this).offset().left,
+    });
 
     $(".fa-close").click(function () {
-      card.remove();
+      menu.remove();
+    });
+
+    $(".show-row").on("click", function () {
+      var selectedData =
+        "فرستنده: " + $row.find("td:nth-child(2)").text() + "<br>";
+      selectedData += "سمت: " + $row.find("td:nth-child(3)").text() + "<br>";
+      selectedData +=
+        "نوع مدرک: " + $row.find("td:nth-child(4)").text() + "<br>";
+      var detailsCard = $('<div id="inbox--details" class="card">')
+        .append(
+          '<div class="card-header"><button class="cancel-btn"><i class="fa fa-close" style="font-size:24px"></i></button></div>'
+        )
+        .append('<div class="card-body">' + selectedData + "</div>");
+      menu.remove();
+
+      $("body").append(detailsCard);
+
+      $(".fa-close").click(function () {
+        detailsCard.remove();
+      });
+    });
+
+    $(".delete-row").click(function () {
+      $row.remove();
+      menu.remove();
+      updateIndexes();
+      closeDetails();
     });
   });
 
-  // -------------
+  function updateIndexes() {
+    $("#inbox--table tbody tr").each(function (index) {
+      $(this)
+        .find("td:first")
+        .text(index + 1);
+    });
+  }
 
-  // delete
+  function closeDetails() {
+    $("#inbox--details").remove();
+    $("#selected-details").remove();
+  }
 
-  $(".delete-row").click(function () {
-    $row.remove();
-    menu.remove();
+  $("#select-all").click(function () {
+    $(".row-checkbox").prop("checked", this.checked);
+    updateActionButtonVisibility();
+  });
+
+  $(document).on("change", ".row-checkbox", function () {
+    updateActionButtonVisibility();
+  });
+
+  $("#delete-selected").click(function () {
+    $(".row-checkbox:checked").closest("tr").remove();
     updateIndexes();
+    updateActionButtonVisibility();
+    closeDetails();
   });
 
-  // -------------
+  $("#view-details").click(function () {
+    var selectedData = "";
+    $(".row-checkbox:checked").each(function () {
+      var $row = $(this).closest("tr");
+      selectedData +=
+        "فرستنده: " + $row.find("td:nth-child(2)").text() + "<br>";
+      selectedData += "سمت: " + $row.find("td:nth-child(3)").text() + "<br>";
+      selectedData +=
+        "نوع مدرک: " + $row.find("td:nth-child(4)").text() + "<br>";
+      selectedData += "<hr>";
+    });
 
+    var detailsCard = $('<div id="selected-details" class="card">')
+      .append(
+        '<div class="card-header"><button class="cancel-btn"><i class="fa fa-close" style="font-size:24px"></i></button></div>'
+      )
+      .append('<div class="card-body">' + selectedData + "</div>");
+    $("body").append(detailsCard);
+
+    $(".fa-close").click(function () {
+      detailsCard.remove();
+    });
+  });
+
+  function updateActionButtonVisibility() {
+    if ($(".row-checkbox:checked").length > 0) {
+      $(".action-buttons").show();
+    } else {
+      $(".action-buttons").hide();
+    }
+  }
 });
-
-
-
-// updateIndexes
-
-function updateIndexes() {
-  $("#inbox--table tbody tr").each(function (index) {
-    $(this)
-      .find("td:first")
-      .text(index + 1);
-  });
-}
-
-// -------------
-
-// dark / light Them
 
 $("#change-them").on("change", function () {
   $("body").toggleClass("dark-theme light-theme", $(this).is(":checked"));
 });
-
-// -------------
-
-// language
 
 $("#languageSelect").change(function () {
   var selectedValue = $(this).val();
@@ -134,5 +166,3 @@ $("#languageSelect").change(function () {
     $("body").removeClass("rtl").addClass("ltr");
   }
 });
-
-// -------------
